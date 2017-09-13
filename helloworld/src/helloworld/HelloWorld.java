f25bc883612e82a631cac01906f6608187538cf3
D071281 <D071281@WDFN32201907A.emea.global.corp.sap>
2017-09-13 15:12:32 +0200

    wrefe

--- helloworld/src/helloworld/HelloWorld.java
+++ helloworld/src/helloworld/HelloWorld.java
@@ -1,25 +1,10 @@
 package helloworld;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+public class HelloWorld {
 
-/**
- * Servlet implementation class HelloWorld
- */
-@WebServlet("/HelloWorld")
-public class HelloWorld extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().println("Hello World!");
+	public static void main(String[] args) {
+		System.out.println("Hello World");
 	}
 
 }
+
